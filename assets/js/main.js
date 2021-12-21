3f8d3b71053bddd6d7ebd0559d57fdf3e41b3897
Rohit Rathod <rr2100599@gmail.com>
2021-12-22 00:55:48 +0530

    added gallery section in index file

--- assets/js/main.js
+++ assets/js/main.js
@@ -178,7 +178,12 @@
 
   });
 
-
+  /**
+   * Initiate glightbox
+   */
+   const glightbox = GLightbox({
+    selector: '.glightbox'
+  });
  
   /**
    * Initiate portfolio lightbox 
@@ -276,6 +281,43 @@
       once: true,
       mirror: false
     });
+  });
+
+  /**
+   * Gallery Slider
+   */
+   new Swiper('.gallery-slider', {
+    speed: 400,
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+      575: {
+        slidesPerView: 2,
+        spaceBetween: 20
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 20
+      },
+      992: {
+        slidesPerView: 5,
+        spaceBetween: 20
+      }
+    }
   });
 
   
